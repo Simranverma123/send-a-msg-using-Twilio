6f06bf01339a71e9547df278cde88db5ecc9d497
Simran Verma <88669720+Simranverma123@users.noreply.github.com>
2022-01-11 20:02:53 +0530

    Added file

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using Twilio;
+using Twilio.Rest.Api.V2010.Account;
+using Twilio.Types;
+
+namespace LearnTwilio
+{    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            var accountSid=Secrets.AccoutSid;
+            var authToken=Secrets.AuthToken;
+            var Tonumber=Secrets.ToNumber;
+            var Fromnumber=Secrets.FromNumber;
+            
+             TwilioClient.Init(accountSid,authToken);
+
+              var message = MessageResource.Create(
+                  to: new PhoneNumber(Tonumber),
+                  from: new PhoneNumber(Fromnumber),
+                  body:"Hi There Thanks for creating and joining LHD"
+              ); 
+              Console.WriteLine(message.Sid);  
+        } 
+    }
+}
